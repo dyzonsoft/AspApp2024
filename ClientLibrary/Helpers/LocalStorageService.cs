77d787868a551c5071a5ace1034a03c04066808d
dyzonsoft <dyzonsoft@gmail.com>
2024-02-18 13:24:55 +0100

    theme

--- ClientLibrary/Helpers/LocalStorageService.cs
+++ ClientLibrary/Helpers/LocalStorageService.cs
@@ -4,9 +4,13 @@ namespace ClientLibrary.Helpers
 {
     public class LocalStorageService(ILocalStorageService localStorageService)
     {
-        private const string StorageKey = "authentication-token";
-        public async Task<string> GetToken() => await localStorageService.GetItemAsStringAsync(StorageKey);
-        public async Task SetToken(string item) => await localStorageService.SetItemAsStringAsync(StorageKey, item);
-        public async Task RemoveToken() => await localStorageService.RemoveItemAsync(StorageKey);
+        private const string TokenKey = "authentication-token";
+        private const string ThemeKey = "theme";
+        public async Task<string> GetToken() => await localStorageService.GetItemAsStringAsync(TokenKey);
+        public async Task SetToken(string token) => await localStorageService.SetItemAsStringAsync(TokenKey, token);
+        public async Task RemoveToken() => await localStorageService.RemoveItemAsync(TokenKey);
+
+        public async Task<string> GetThemeAsync() => await localStorageService.GetItemAsStringAsync(ThemeKey);
+        public async Task SetThemeAsync(string theme) => await localStorageService.SetItemAsStringAsync(ThemeKey, theme);
     }
 }
