cc6d7ee376b11ddd1061ac25e7815304df9c43f0
dyzonsoft <dyzonsoft@gmail.com>
2024-02-24 14:54:01 +0100

    konec

---
+++ Clientela/Program.cs
@@ -0,0 +1,16 @@
+using Blazored.LocalStorage;
+using Clientela;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
